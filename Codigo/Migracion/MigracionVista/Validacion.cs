17b2b2ec031aa70bf1bfa8aa874e79f5fc0b7957
Edward8585 <110178067+Edward8585@users.noreply.github.com>
2023-02-23 14:33:00 -0600

    Update Validacion.cs

--- Codigo/Migracion/MigracionVista/Validacion.cs
+++ Codigo/Migracion/MigracionVista/Validacion.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +15,26 @@ namespace MigracionVista
         public Validacion()
         {
             InitializeComponent();
+        }
+
+        private void Validacion_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
